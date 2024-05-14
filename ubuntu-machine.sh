c958c472e396d3d7eae56418d1ac6506dc747ffd
Marcos Vinicius <omarcosviniciusdev@gmail.com>
2024-05-14 18:40:56 -0300

    refactor: change sudo su to just sudo

--- ubuntu-machine.sh
+++ ubuntu-machine.sh
@@ -1,17 +1,15 @@
 # Ubuntu Machine prepare
 
-sudo su
+sudo apt update -y && apt upgrade -y
 
-apt update -y && apt upgrade -y
-
-apt autoremove -y && apt clean -y
+sudo apt autoremove -y && apt clean -y
 
 echo "Will install: make, docker, docker-compose, unzip, and build-essential"
 echo "Install this things? [Y,n]"
 read input
 if [[ $input == "Y" || $input == "y" ]]; then
         echo "Installing..."
-	apt-get install ca-certificates curl gnupg
+	sudo apt-get install ca-certificates curl gnupg
 	install -m 0755 -d /etc/apt/keyrings
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 	chmod a+r /etc/apt/keyrings/docker.gpg
@@ -19,16 +17,18 @@ if [[ $input == "Y" || $input == "y" ]]; then
 	  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
 	  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
 	  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-	apt install install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-	apt install -y make docker.io docker-compose unzip build-essential curl
-	groupadd docker
-	gpasswd -a $USER docker
+	sudo apt install install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
+	sudo curl -SL https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+	chmod +x /usr/local/bin/docker-compose
+	sudo apt install -y make docker.io docker-compose unzip build-essential curl
+	sudo groupadd docker
+	sudo gpasswd -a $USER docker
 	newgrp docker
 
 	echo "Hello world from docker: "
 	docker run hello-world
-	apt update -y && apt upgrade -y
-	apt autoremove -y && apt clean -y
+	sudo apt update -y && apt upgrade -y
+	sudo apt autoremove -y && apt clean -y
 else
 	clear
 	echo "Exiting"
